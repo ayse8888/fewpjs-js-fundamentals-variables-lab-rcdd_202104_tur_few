5b04ca19f3a1e85df03156f8954692c9f9f0f36f
Ayse Cimen Basar <aysebasar91@gmail.com>
2021-04-18 18:09:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solution in this file!
+<<<<<<< HEAD
 const companyName = 'Scuber';
 let mostProfitableNeighborhood = 'Chelsea';
 let companyCeo = 'Susan Smith';
+=======
+const companyName = 'Scuber';
+>>>>>>> 3f762c9b88fb80c1077a3ee82e1d16716defe79a
